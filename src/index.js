98ed9810fcb35cebb96d2fb5e31979bf226b4d26
kangdongxia <2557381271@qq.com>
2020-07-08 22:26:16 +0800

    做了简单练习

--- src/index.js
+++ src/index.js
@@ -3,11 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// function formatName(user){
+//   return user.firstName + ' ' + user.lastName;
+// }
+// const user = {
+//   firstName:'111',
+//   lastName:'888888'
+// }
+// const name = 'Josh Perez';
+// const element = <h1>Hello,{formatName(user)}!</h1>;
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+// const element = <Welcome name="Sara" />;
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number}>{number}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
 
+const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //    <App /> 
+  // </React.StrictMode>,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
 
